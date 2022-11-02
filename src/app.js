77b91e7fcd84f22e510b0de33f1976831ca9c386
pedrodcarvalho <pedrodc51203@gmail.com>
2022-11-02 18:10:10 +0100

    Minor code change

--- src/app.js
+++ src/app.js
@@ -7,30 +7,31 @@ const hostname = process.env.HOSTNAME || 'localhost';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    if (req.url === `/weather?city=${req.url.split('=')[1]}`) {
-        return getWeather(req, res);
-    }
-
-    switch (req.url.split('.')[1]) {
-        case undefined:
-            if (req.url === '/') {
-                sendFile(res, '/index.html', 'text/html');
-                break;
-            }
-        case 'html':
-            sendFile(res, req.url, 'text/html');
-            break;
-        case 'css':
-            sendFile(res, req.url, 'text/css');
-            break;
-        case 'js':
-            sendFile(res, req.url, 'text/javascript');
-            break;
-        case 'png':
-            sendFile(res, req.url, 'image/png');
-            break;
-        default:
+    if (req.url.split('.')[1] === undefined) {
+        if (req.url === '/') {
+            sendFile(res, '/index.html', 'text/html');
+        }
+        else if (req.url.split('?')[0] === '/weather') {
+            getWeather(req, res);
+        }
+        else {
             sendFile(res, '/pages/404.html', 'text/html');
+        }
+    }
+    else if (req.url.split('.')[1] === 'html') {
+        sendFile(res, req.url, 'text/html');
+    }
+    else if (req.url.split('.')[1] === 'css') {
+        sendFile(res, req.url, 'text/css');
+    }
+    else if (req.url.split('.')[1] === 'js') {
+        sendFile(res, req.url, 'text/javascript');
+    }
+    else if (req.url.split('.')[1] === 'png') {
+        sendFile(res, req.url, 'image/png');
+    }
+    else {
+        sendFile(res, '/pages/404.html', 'text/html');
     }
 });
 
--- src/routes/apis/weather.js
+++ src/routes/apis/weather.js
@@ -3,10 +3,10 @@ const http = require('node:http');
 const getWeather = (req, res) => {
     const CITY = req.url.split('=')[1].replace(' ', '+');
 
-    http.get(`http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${process.env.WEATHER_API_KEY}&units=metric`, (resp) => {
+    http.get(`http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${process.env.WEATHER_API_KEY}&units=metric`, (response) => {
         let data = '';
 
-        resp.on('data', (chunk) => {
+        response.on('data', (chunk) => {
             data += chunk;
         }).on('end', () => {
             return res.end(data);
